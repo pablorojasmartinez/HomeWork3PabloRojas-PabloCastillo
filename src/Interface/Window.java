1bb00b6d8e61793183a604760d4744f7e5c81a4b
Pablo Rojas M <Pablo Rojas M@10.234.223.72>
2018-04-16 14:07:00 -0600

    3th homework, Programmation II

---
+++ src/Interface/Window.java
@@ -0,0 +1,100 @@
+package Interface;
+
+import java.util.Random;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Window extends Application implements Runnable {
+
+    private final int WIDTH = 800;
+    private final int HEIGHT = 600;
+    private Pane pane;
+    private Scene scene;
+    private Canvas canvas;
+    private Thread thread;
+    int x;
+    int y;
+    private Object MessageBox;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Random Graphics");
+        initComponents(primaryStage);
+        primaryStage.show();
+    }
+
+    private void initComponents(Stage primaryStage) {
+        this.pane = new Pane();
+        this.scene = new Scene(this.pane, WIDTH, HEIGHT);
+        this.canvas = new Canvas(WIDTH, HEIGHT);
+
+        this.thread = new Thread(this);
+        this.thread.start();
+
+        this.pane.getChildren().add(this.canvas);
+        primaryStage.setScene(this.scene);
+        //primaryStage.setOnCloseRequest(exit);
+    }
+
+    private void myDraw(GraphicsContext gc) {
+        Random rand = new Random();
+        int x = 0;
+        int y = 0;
+        while (true) {
+            int rand1 = rand.nextInt(5 - 1) + 1;
+            try {
+                gc.clearRect(0, 0, WIDTH, HEIGHT);
+                gc.setFill(Color.AQUA);
+
+                if (rand1 == 1) {
+                    gc.fillRect((x), y, 50, 50);
+                    x += 6;
+                    if (x > WIDTH) {
+                        x -= 100;
+                    }
+                } else if (rand1 == 2) {
+                    gc.fillRect((x), y, 50, 50);
+                    x -= 6;
+                    if (x < 0) {
+                        x += 100;
+
+                    }
+
+                } else if (rand1 == 3) {
+                    gc.fillRect((x) + 1, y, 50, 50);
+                    y += 6;
+                    if (y > this.HEIGHT) {
+                        y -= 100;
+                    }
+
+                } else {
+                    gc.fillRect((x), y, 50, 50);
+                    y -= 6;
+                    if (y < 0) {
+                        y += 100;
+
+                    }
+                }
+
+                Thread.sleep(100);
+
+            } catch (InterruptedException ex) {
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        GraphicsContext gc = this.canvas.getGraphicsContext2D();
+        myDraw(gc);
+    }
+
+    public static void main(String[] args) {
+        Application.launch(Window.class, args);
+    }
+}
---
+++ src/lineexample/LineExample.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lineexample;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Pablo Rojas Martínez
+ */
+public class LineExample extends JPanel {
+
+    public LineExample() {
+        this.setPreferredSize(new Dimension(800, 600));
+    } // constructor
+
+    private void draw(Graphics g) throws InterruptedException {
+
+        drawLines(g);
+
+    } // draw
+
+    public void drawLines(Graphics g) throws InterruptedException {
+
+        int i = 0;
+
+        while (i <1000) {
+
+            int red = (int) ((Math.random() * 255) + 0);
+            int green = (int) ((Math.random() * 255) + 0);
+            int blue = (int) ((Math.random() * 255) + 0);
+
+            Color color = new Color(red, green, blue);
+            g.setColor(color);
+
+//            System.out.println(red + " " + green + " " + blue);
+//
+//            System.err.println(i);
+            //x0 y y0 son los puntos de inicio
+            //x1 y y1 son los puntos de llegada
+            int x0 = (int) ((Math.random() * this.getWidth())); // Para que haga números no superiores al ancho
+            int y0 = (int) ((Math.random() * this.getHeight())); // Para que haga números no superiores al alto
+
+            int x1 = (int) ((Math.random() * this.getWidth())); // Para que haga números no superiores al ancho
+            int y1 = (int) ((Math.random() * this.getHeight()));// Para que haga números no superiores al alto
+
+            int pointX = x1 - x0;
+            int pointY = y1 - y0;
+
+//            System.out.println("x1:" + x0 + " " + "x2:" + x1 + " " + "y1:" + y0 + " " + "y2:" + y1);
+//            System.out.println("dx: " + pointX + " dy: " + pointY + "\n");
+          //  g.drawLine(x0, y0, x0, y0);
+
+            if (Math.abs(pointX) > Math.abs(pointY)) {          // Si la pendiente es negativa 
+                float m = (float) pointY / (float) pointX;
+                float b = y0 - m * x0;
+                if (pointX < 0) {
+                    pointX = -1;
+                } else {
+                    pointX = 1;
+                }
+                while (x0 != x1) {
+
+                    x0 += pointX;
+                    y0 = Math.round(m * x0 + b);
+                    g.drawLine(x0, y0, x0, y0);
+
+                }
+            } else if (pointY != 0) {                              //Si la pendiente es positiva 
+                float m = (float) pointX / (float) pointY;
+                float b = x0 - m * y0;
+                if (pointY < 0) {
+                    pointY = -1;
+                } else {
+                    pointY = 1;
+                }
+                while (y0 != y1) {
+                    y0 += pointY;
+                    x0 = Math.round(m * y0 + b);
+                    g.drawLine(x0, y0, x0, y0);
+
+                }
+            }
+            i++;
+
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        try {
+            super.paintComponent(g);
+            draw(g);
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(LineExample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Graphing Function");
+        window.setContentPane(new LineExample());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.pack();
+        window.setResizable(false);
+        window.setLocation(180, 50);
+        window.setVisible(true);
+    }
+
+} // fin clase
+
+// Algoritmo tomado de las páginas: 
+//http://galia.fc.uaslp.mx/~medellin/Applets/LineasRectas/Recta.htm
+//http://graficacion2016b.blogspot.com/2016/08/deltax-deltay-m-algoritmo-de-bresenham.html
+//https://www.scribd.com/document/105561908/Algoritmo-Dda-Bresenham-JAVA
